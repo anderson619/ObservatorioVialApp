7040bd5a60bd9e082b8f7d14e09844b97e51f51c
Anderson <andersonmantuano@unicomfacauca.edu.co>
2025-05-24 13:10:14 -0500

    dbrouters.py

---
+++ config/dbrouters.py
@@ -0,0 +1,20 @@
+class YoloRouter:
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'yolo':
+            return 'yolo_db'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'yolo':
+            return 'yolo_db'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'yolo' or obj2._meta.app_label == 'yolo':
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == 'yolo':
+            return db == 'yolo_db'
+        return db == 'default'
