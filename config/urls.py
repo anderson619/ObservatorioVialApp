daf82d59730c3da1c634221b507e88f7897fff17
Anderson <andersonmantuano@unicomfacauca.edu.co>
2025-05-30 11:42:05 -0500

    froned

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LoginView.as_view(), name='logout'),
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
@@ -13,3 +13,4 @@ urlpatterns = [
     path('', include('usuarios.urls')),
     path('yolo/', include('yolo.urls')),
 ]
+
